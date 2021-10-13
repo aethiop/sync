c4ca17e5f5b40e4940475bb1e512ed9f675feaff
Natnael Teferi <pinobirhanu3@gmail.com>
2021-10-13 08:59:15 -0500

    Navbar update

--- src/components/molecules/IconLink.svelte
+++ src/components/molecules/IconLink.svelte
@@ -12,5 +12,5 @@
 	class="flex flex-col justify-center w-full cursor-pointer space-x-4 rounded-lg px-2 py-2 sm:px-4 sm:py-1  hover:text-primary  sm:flex-row items-center"
 >
 	<Icon name={icon} fill={active} />
-	<p class="w-full">{name}</p>
+	<p class="hidden sm:block w-full">{name}</p>
 </div>
