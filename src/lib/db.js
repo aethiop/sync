e9ac90bc4ac27a0abbf35d33a6f1a5d8d5679108
Natnael Teferi <pinobirhanu3@gmail.com>
2021-10-25 13:59:43 -0500

    updated peer

--- src/lib/db.js
+++ src/lib/db.js
@@ -9,11 +9,7 @@ import "gun/lib/radisk";
 GUN.log.off = true;
 import { writable } from "svelte/store";
 export const gun = GUN({
-	peers: [
-		"http://localhost:8765/gun",
-		"https://marda.herokuapp.com/gun",
-		"http://gun-matrix.herokuapp.com/gun",
-	],
+	peers: ["http://localhost:8765/gun", "https://marda.herokuapp.com/gun"],
 	localStorage: false,
 });
 export const user = gun.user().recall({ sessionStorage: true });
