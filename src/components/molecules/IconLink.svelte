afa28065c88b342a92c01305c603e325c3676e19
Natnael Teferi <pinobirhanu3@gmail.com>
2021-10-25 18:49:27 -0500

    mobile fix

--- src/components/molecules/IconLink.svelte
+++ src/components/molecules/IconLink.svelte
@@ -1,7 +1,6 @@
 <script>
 	import Icon from "$atoms/Icon.svelte";
 	import Text from "$atoms/Text.svelte";
-
 	export let active;
 	export let icon;
 	export let name;
@@ -13,7 +12,7 @@
 		" flex justify-start transition-transform ease-out w-full text-on-background cursor-pointer space-x-4 rounded-lg px-2 py-2 sm:px-4 sm:py-1    flex-row items-center"}
 >
 	<Icon name={icon} fill={active} />
-	<span class:hidden={!active} class=" lg:block pr-2 ">
+	<span class="hidden lg:block pr-2 ">
 		<Text type="body">{name}</Text>
 	</span>
 	<!-- <p class=" sm:block w-full pr-2">{name}</p> -->
